3ba772da385e63d563197910dbaaadd8ddd5767e
Pankaj Chouhan <pankajchouhan80@gmail.com>
2023-07-25 12:28:56 +0000

    basic

---
+++ Basic /basic1.js
@@ -0,0 +1,52 @@
+console.log("welcome.....")
+
+// variable ...
+
+Name = "joy";
+age = 12;
+height = 5.6;
+
+console.log("My name is  : ",Name);
+console.log("My age is  : ",age);
+console.log("My height is  : ",height);
+
+let msg = "codeswithpankaj";
+
+var textmsg = "p4n.in"
+
+Name = "Toy"
+
+console.log(Name)
+
+const a = 500;
+
+console.log(a)
+
+
+msg1 = [
+    item1=400,
+    item2 =700,
+]
+
+console.log(msg1)
+
+
+// access item 
+console.log("item1 = ------- ",msg1[0])
+
+
+num = [78,88,55,22,44]
+
+console.log(num[2])
+
+console.log(num)
+
+r = {
+
+    website1:"p4n.in",
+    website2:"codeswithpankaj.com"
+}
+
+
+console.log(r.website1)
+console.log(r.website2)
